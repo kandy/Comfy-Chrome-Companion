8b3f77178f6db2c007f5c692094cdd5be7ebcd8b
Andrii Kasian <akasian@adobe.com>
2024-09-06 16:45:28 -0500

    Update workflow with  url

--- background.js
+++ background.js
@@ -17,12 +17,17 @@ chrome.contextMenus.onClicked.addListener((info, tab) => {
       const clientId = items.clientId;
       const workflowData = items.workflow ? JSON.parse(items.workflow) : {};
 
+      // Define the JSON payload for the ConfyUI API
+      let workflow = workflowData;
+      //index = workflow.find((el) => el.type == "Load Image From Url (mtb)");
+      workflow["143"].inputs = {"url": imageUrl};
+
       fetch(url, {  
         method: 'POST',
         headers: {
           'Content-Type': 'application/json'
         },
-        body: JSON.stringify(workflowData)
+        body: JSON.stringify( {"prompt": workflow, "client_id": clientId})
       })
       .then(response => response.json())
       .then(data => console.log('Success:', data))
